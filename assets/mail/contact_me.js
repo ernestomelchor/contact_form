b83d8f44194ece62219a86e94a6788c2381e2880
ernestomelchor <nurturelove88@gmail.com>
2020-08-26 22:56:50 -0500

    updated url

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -21,7 +21,7 @@ $(function () {
             $this = $('#sendMessageButton');
             $this.prop('disabled', true); // Disable submit button until AJAX call is complete to prevent duplicate messages
             $.ajax({
-                url: 'https://ernestomelchor.github.io/contact_form/',
+                url: 'http://ernestomelchor.github.io/contact_form/',
                 type: 'POST',
                 data: {
                     name: name,
